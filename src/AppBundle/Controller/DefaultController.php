65c25ae40018cd73432492fedbc0a16c08869eff
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-12-30 23:11:54 +0100

    Refactoring pour la classe  FFF
    apparement la placer dans le répertoire src empcêhe Symphony de la
    chager automatiquement.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,8 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use AppBundle\Utility\FFF;
+use Utility\FFF;
+
 
 
 class DefaultController extends Controller
@@ -18,7 +19,7 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             //'base_dir' => realpathV($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-            'base_dir' => \utility\FFF::realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => FFF::realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
    
